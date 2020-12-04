eefcae494956bfb5d8baf3bab333371137ee9a73
Alexander <32229551+TimofeevAlex@users.noreply.github.com>
2020-12-05 00:04:34 +0100

    Add landmarks to visualization

--- src/visualization_helpers.py
+++ src/visualization_helpers.py
@@ -46,7 +46,68 @@ def plot_by_groups(data, colours, type_name='colours', opacity=1.0, name=None, s
         fig.write_html(name + ".html")
     fig.show()
     
-def plot_transposition_with_centers(data, transposition, major_minor, text, assignments=None, type_name='colours', opacity=0.7, size=4, save=True):
+profiles = {
+    "metadata": {
+        "major": [0.16728176182048993, 0.02588894338557735, 0.1171404498466961, 0.0356495908305276,
+                  0.11123089694868556, 0.09004464377374272, 0.0382659859401006, 0.16751964441157266,
+                  0.03408440554363338, 0.09272715883619839, 0.03950089152204068, 0.08066562714073418],
+        "minor": [0.1556462295111984, 0.03462755036633632, 0.09903157107324087, 0.1044334346698825,
+                  0.048477125346135526, 0.10397079256257924, 0.034020023696857145, 0.15515432119981398,
+                  0.07456760938348618, 0.05334836234206381, 0.08307367815831439, 0.05364930169009233]},
+    "bayes": {
+        "major": [0.19965448155346477, 0.008990613278155785, 0.12994202213313824, 0.01242816337359103,
+                  0.12638864214266632, 0.08545131087389726, 0.0263734257934788, 0.20655914875192785,
+                  0.013393451149384972, 0.08825555654425607, 0.01458718179430356, 0.08797600261173529],
+        "minor": [0.17930236689047313, 0.017073963004507538, 0.10822708838359425, 0.11953464160572477,
+                  0.025539463722006557, 0.11184032733554274, 0.020187990226227148, 0.181798715257532,
+                  0.07218923098556179, 0.0341394852891066, 0.08257135177438128, 0.047595375525342154]},
+    "krumhansl": {
+        "major": [0.15195022732711172, 0.0533620483369227, 0.08327351040918879, 0.05575496530270399,
+                  0.10480976310122037, 0.09787030390045463, 0.06030150753768843, 0.1241923905240488,
+                  0.05719071548217276, 0.08758076094759511, 0.05479779851639147, 0.06891600861450106],
+        "minor": [0.14221523253201526, 0.06021118849696697, 0.07908335205571781, 0.12087171422152324,
+                  0.05841383958660975, 0.07930802066951245, 0.05706582790384183, 0.1067175915524601,
+                  0.08941810829027184, 0.06043585711076162, 0.07503931700741405, 0.07121995057290496]},
+    "temperley": {
+        "major": [0.12987012987012989, 0.05194805194805195, 0.09090909090909091, 0.05194805194805195,
+                  0.1168831168831169, 0.1038961038961039, 0.05194805194805195, 0.1168831168831169,
+                  0.05194805194805195, 0.09090909090909091, 0.03896103896103896, 0.1038961038961039],
+        "minor": [0.12987012987012989, 0.05194805194805195, 0.09090909090909091, 0.1168831168831169,
+                  0.05194805194805195, 0.1038961038961039, 0.05194805194805195, 0.1168831168831169,
+                  0.09090909090909091, 0.05194805194805195, 0.03896103896103896, 0.1038961038961039]},
+    "albrecht": {
+        "major": [0.23800000000000004, 0.006000000000000002, 0.11100000000000003, 0.006000000000000002,
+                  0.13700000000000004, 0.09400000000000003, 0.016000000000000004, 0.21400000000000005,
+                  0.009000000000000001, 0.08000000000000002, 0.008000000000000002, 0.08100000000000002],
+        "minor": [0.22044088176352702, 0.0060120240480961915, 0.10420841683366731, 0.12324649298597193,
+                  0.019038076152304607, 0.10320641282565128, 0.012024048096192383, 0.21442885771543083,
+                  0.06212424849699398, 0.022044088176352703, 0.06112224448897795, 0.05210420841683366]}
+}
+
+notes = ['C Major', 'C Minor', 'C# Major', 'C# Minor', 'D Major', 'D Minor',  
+         'D# Major', 'D# Minor', 'E Major', 'E Minor', 'F Major', 'F Minor',  
+         'F# Major', 'F# Minor', 'G Major', 'G Minor', 'G# Major', 'G# Minor',  
+         'A Major', 'A Minor', 'A# Major', 'A# Minor', 'B Major', 'B Minor']
+
+def get_landmarks(landmark_name):
+    '''
+    Returns all possible transpositions of a given profile
+    Parameters:
+    - landmark_name: str
+      A name of a profile
+    Return:
+    - landmarks: array-like
+      An array of all possible transpositions (12 major and 12 minor) for a given landmark_name
+    '''
+    num_transp = 12
+    orig_profile = np.stack([profiles[landmark_name]['major'], profiles[landmark_name]['minor']], axis=0).astype(np.float64)
+    transp_landmarks = orig_profile
+    for i in range(1, num_transp):
+        transp_landmarks = np.concatenate([transp_landmarks, np.roll(orig_profile, i, -1)], axis=0)
+    return transp_landmarks  
+
+def plot_transposition_with_centers(data, transposition, major_minor, text, assignments=None, landmarks=False, \
+                                    landmark_name='albrecht', type_name='colours', opacity=0.7, size=4, save=True):
     '''
     Plots data points in 3D space colorizing them by their transposition as well as centers of the found clusters.
     Both major and minor parts of points can be viewed either simultaneously or separately. 
@@ -62,6 +123,12 @@ def plot_transposition_with_centers(data, transposition, major_minor, text, assi
         - assignments: array-like (number of samples, )
           An array of integer numbers that assigns points to a certain cluster. 
           If None is given, it doesn't plot centers. Default: None
+        - landmarks: bool
+          If this parameter is True, it plots landmarks for a chosen in landmark_name
+          profile. Default: False
+        - landmark_name: str
+          It defines a profile which is used for plotting landmarks. 
+          Options: "metadata", "bayes", "krumhansl", "temperley", "albrecht"
         - type_name: str
           A name that characterizes given groups. Default: 'colours'
         - opacity: float or array-like (number of samples, )
@@ -72,8 +139,8 @@ def plot_transposition_with_centers(data, transposition, major_minor, text, assi
         - save: bool
           It saves the plot in HTML format when the parameter is True. Default: True
     '''
-    
-    data_proj = Isomap(n_components=3).fit_transform(data) # gets projected data by means of Isomap
+    isomap = Isomap(n_components=3)
+    data_proj = isomap.fit_transform(data) # gets projected data by means of Isomap
     # Forms major and minor DataFrames
     df_major = pd.DataFrame({'note': transposition[major_minor==1], 'x': data_proj[major_minor==1, 0], \
                        'y': data_proj[major_minor==1, 1], 'z': data_proj[major_minor==1, 2], 'text': text[major_minor==1]})
@@ -93,10 +160,16 @@ def plot_transposition_with_centers(data, transposition, major_minor, text, assi
         centers = np.array(centers)
         centers = pd.DataFrame({'x': centers[:, 0], 'y': centers[:, 1], 'z': centers[:, 2]})
         fig_data.append(go.Scatter3d(x=centers['x'], y=centers['y'], z=centers['z'], mode='markers', text='Center', \
-                                     marker=dict(size=12, colorscale='Viridis', color=np.arange(centers.shape[0]),  \
+                                     marker=dict(size=10, colorscale='Viridis', color=np.arange(centers.shape[0]),  \
                                      symbol='diamond', opacity=1.0)))
         name += '_with_centers_' + str(len(centers))
-        
+    if landmarks:
+        landmarks = isomap.transform(get_landmarks(landmark_name))
+        landmarks = pd.DataFrame({'x': landmarks[:, 0], 'y': landmarks[:, 1], 'z': landmarks[:, 2]})
+        fig_data.append(go.Scatter3d(x=landmarks['x'], y=landmarks['y'], z=landmarks['z'], mode='markers+text', text=notes, \
+                                     marker=dict(size=7, color='black',  \
+                                     symbol='cross', opacity=1.0)))
+        name += '_with_landmarks_' + str(len(landmarks))
     fig = go.Figure(data=fig_data)
     fig.update_layout()
     if save:
